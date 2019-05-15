0d6e2ee44a4366d7dbae97264cdfb84186c69f1f
Joe Baragona <jrb5004@gmail.com>
2019-05-15 16:24:51 -0700

    updated map

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
-const searchURL = 'https://api.data.gov/ed/collegescorecard/v1/schools';
+const collegeSearchUrl = 'https://api.data.gov/ed/collegescorecard/v1/schools';
+const mapsSearchUrl = 'https://maps.googleapis.com/maps/api/staticmap';
 const scorecardApiKey = 'SzC7hjHyOhLpPQI5hy0Qfm6Ed5PLKtNWCsnfHeIY';
+const mapsApiKey = 'AIzaSyA_sY_dRcWOXDYugaPqd7QDTbXviHFnLVc';
+let map;
 
 function getColleges(zip, radius) {
     const paramString = `api_key=${scorecardApiKey}&school.degrees_awarded.predominant=3&_zip=${zip}&_distance=${radius}mi&latest.student.size__range=1500..&_per_page=100&_sort=latest.student.size:desc&_fields=id,school.name,school.city,school.state,id,latest.student.size,school.school_url,latest.admissions.sat_scores.average.overall,latest.admissions.admission_rate.overall,latest.cost.tuition.in_state,latest.cost.tuition.out_of_state,latest.completion.rate_suppressed.four_year_100_pooled,latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings,location.lon,location.lat`;
-    const url = searchURL + '?' + paramString;
+    const collegesUrl = collegeSearchUrl + '?' + paramString;
 
-    fetch(url)
+    fetch(collegesUrl)
     .then(response => {
         if (response.ok) {
             return response.json();
@@ -38,6 +41,13 @@ function displayResults(responseJson) {
                 <th scope="col">Average Salary - Recent Grads</th>
             </tr>`)
     for (let i = 0; i < responseJson.results.length; i++) {
+
+        var latLng = new google.maps.LatLng(responseJson.results[i]['location.lat'],responseJson.results[i]['location.lon']);
+        var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+        });
+
         $('.results-table').append(
             `<tr>
                 <th scope="row"><a href="http://${responseJson.results[i]['school.school_url']}" target="_blank">${responseJson.results[i]['school.name']}</a></th>
@@ -52,6 +62,14 @@ function displayResults(responseJson) {
             </tr>`
         )};
     $('.results').removeClass('hidden');
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 2,
+      center: new google.maps.LatLng(2.8,-187.3),
+      mapTypeId: 'terrain'
+    });
 }
 
 function watchInput() {
