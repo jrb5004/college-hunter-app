44a50e544b61e7d92a64a955b12b8280b1b78b3f
Joe Baragona <jrb5004@gmail.com>
2019-05-22 15:24:56 -0700

    added footer, removed header image, formatted numbers

--- index.js
+++ index.js
@@ -62,13 +62,13 @@ function displayResults(responseJson) {
             `<tr>
                 <th scope="row"><a href="http://${responseJson.results[i]['school.school_url']}" target="_blank">${responseJson.results[i]['school.name']}</a></th>
                 <td>${responseJson.results[i]['school.city']}, ${responseJson.results[i]['school.state']}</td>
-                <td>${responseJson.results[i]['latest.admissions.admission_rate.overall']}</td>
+                <td>${convertToPercent(responseJson.results[i]['latest.admissions.admission_rate.overall'])}</td>
                 <td>${responseJson.results[i]['latest.admissions.sat_scores.average.overall']}</td>
-                <td>${responseJson.results[i]['latest.student.size']}</td>
-                <td>${responseJson.results[i]['latest.cost.tuition.in_state']}</td>
-                <td>${responseJson.results[i]['latest.cost.tuition.out_of_state']}</td>
-                <td>${responseJson.results[i]['latest.completion.rate_suppressed.four_year_100_pooled']}</td>
-                <td>${responseJson.results[i]['latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings']}</td>
+                <td>${formatNumbers(responseJson.results[i]['latest.student.size'])}</td>
+                <td>$${formatNumbers(responseJson.results[i]['latest.cost.tuition.in_state'])}</td>
+                <td>$${formatNumbers(responseJson.results[i]['latest.cost.tuition.out_of_state'])}</td>
+                <td>${convertToPercent(responseJson.results[i]['latest.completion.rate_suppressed.four_year_100_pooled'])}</td>
+                <td>$${formatNumbers(responseJson.results[i]['latest.earnings.6_yrs_after_entry.working_not_enrolled.mean_earnings'])}</td>
             </tr>`
         )};
     $('.results').removeClass('hidden');
@@ -81,6 +81,17 @@ function formatResultsPage() {
     $('.error-message').remove();
     $('main').removeClass('home');
     $('main').addClass('paper');
+}
+
+function convertToPercent(numberToConvert) {
+    let percentage = (numberToConvert * 100).toFixed(0) 
+    if (isNaN(percentage)) return 'NA'
+    else return percentage + '%'
+}
+
+function formatNumbers(numberToFormat) {
+    let formattedNumber = numberToFormat.toLocaleString('en')
+    return formattedNumber
 }
 
 function initMap(responseJson) {
